9ce87653023c5336d6a975191c8ba8411b8d1b14
Dhruv Mathur <dhruvm@icloud.com>
2022-07-08 00:56:36 -0400

    Initial Commit

---
+++ SonicSnail/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  SonicSnail
+//
+//  Created by Dhruv Mathur on 2022-07-08.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
